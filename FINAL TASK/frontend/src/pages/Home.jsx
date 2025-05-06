e90caf30ac49cd084a13040bf1a77ad43ca5f797
GoncziTibor <79456470+GoncziTibor@users.noreply.github.com>
2025-05-06 12:46:11 +0200

    7.commit
    register

--- FINAL TASK/frontend/src/pages/Home.jsx
+++ FINAL TASK/frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const Home = () => {
-    
+  
   return (
-<div className='text-3xl text-blue-500'>Home</div>  )
-
+    <div className='text-3xl text-blue-500'>Home</div>
+  )
 }
 
 export default Home
